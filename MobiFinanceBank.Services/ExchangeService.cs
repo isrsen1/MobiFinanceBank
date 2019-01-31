a3b26cfaf3d24cd5c11f35f9fac6d46ad819dc89
Ivan Sršen <ivan.srsen@enterwell.net>
2019-01-31 08:45:17 +0100

    Exchange service

---
+++ MobiFinanceBank.Services/ExchangeService.cs
@@ -0,0 +1,54 @@
+﻿using MobiFinanceBank.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobiFinanceBank.Services
+{
+    /// <summary>
+    /// Exchange service
+    /// </summary>
+    public class ExchangeService
+    {
+        public string BaseUri { get; set; } = "http://api.hnb.hr/tecajn/v1";
+        public HttpResponseMessage CurrentAllCurrencyExchangeRate(IEnumerable<Currency> currencies, DateTime? fromDate, DateTime? toDate)
+        {
+            string parameters = "?";
+
+            if (currencies != null && currencies.Any())
+            {
+                foreach (var currency in currencies)
+                {
+                    parameters += $"valuta={currency}&";
+                }
+            }
+            
+            if (fromDate != null)
+                parameters += $"datum-od={String.Format("{0:yyyy-MM-dd}", fromDate)}&";
+
+            if(toDate != null)
+                parameters += $"datum-do={String.Format("{0:yyyy-MM-dd}", toDate)}&";
+
+            // using http client for hnb api
+            using (HttpClient client = new HttpClient())
+            {
+                // Setting base address and sending get request
+                client.BaseAddress = new Uri(this.BaseUri);
+                var response = client.GetAsync(parameters).Result;
+
+                //string result = response.Content.ReadAsStringAsync().Result;
+
+                // if response status ok, return response
+                if (response.IsSuccessStatusCode)
+                    return response;
+
+                // return bad request
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            }
+        }
+    }
+}
---
+++ MobiFinanceBank/Vm/CurrencyExchangeRate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MobiFinanceBank.Model.Enums;
+using Newtonsoft.Json;
+
+namespace MobiFinanceBank.Vm
+{
+    public class CurrencyExchangeRate
+    {
+        [JsonProperty("Broj tečajnice")]
+        public string ExchangeRateList { get; set; }
+
+        [JsonProperty("Datum primjene")]
+        public DateTime ApplicationDate { get; set; }
+
+        [JsonProperty("Država")]
+        public string Country { get; set; }
+
+        [JsonProperty("Šifra valute")]
+        public int CurrencyCode { get; set; }
+
+        [JsonProperty("Valuta")]
+        public Currency Currency { get; set; }
+
+        [JsonProperty("Jedinica")]
+        public int Unit { get; set; }
+
+        [JsonProperty("Kupovni za devize")]
+        public double Buying { get; set; }
+
+        [JsonProperty("Srednji za devize")]
+        public double Middle { get; set; }
+
+        [JsonProperty("Prodajni za devize")]
+        public double Selling { get; set; }
+    }
+}
